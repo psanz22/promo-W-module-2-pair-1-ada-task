623bbf73cbbf6085721ff637874cd2a4ed3b5044
Paula <psanzilundain@gmail.com>
2024-02-28 21:02:40 +0100

    hola

--- prueba.js
+++ prueba.js
@@ -20,12 +20,25 @@ const tasks = [
 const renderTasks = () => {
     for(let task of tasks){
         if(task.completed){
-            taskList.innerHTML += `<li><input type="checkbox" class="checked"> ${task.name}</li>`
+            taskList.innerHTML += `<li class="checked"><input id="${task.name}"type="checkbox" checked> ${task.name}</li>`
 
         }else {
-            taskList.innerHTML += `<li><input type="checkbox"> ${task.name}</li>`
+            taskList.innerHTML += `<li><input type="checkbox" id="${task.name}"> ${task.name}</li>`
 
         }
     }
 }
 renderTasks();
+
+const handleCheck = (event) => {
+    const inputId = event.target.id;
+    console.log(inputId);
+    const taskIndex = tasks.findIndex((task) => {
+    return task.name === inputId;
+})
+console.log(taskIndex);
+    tasks[taskIndex].completed = event.target.checked;
+    console.log(tasks);
+}
+renderTasks(task);
+taskList.addEventListener('click', handleCheck);
