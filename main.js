932e63a9c0a22a0017a9bca74774e14574a33c97
Paula <psanzilundain@gmail.com>
2024-02-29 11:24:13 +0100

    Añadir nueva tarea

--- main.js
+++ main.js
@@ -3,7 +3,7 @@
 const GITHUB_USER = '<lejladzanko>';
 const SERVER_URL = `https://dev.adalab.es/api/todo/${GITHUB_USER}`;
 
-const tasks = [];
+let tasks = [];
 
 const list = document.querySelector('.js-list');
 const inputNewTask = document.querySelector('.js-input-new-task');
@@ -24,50 +24,31 @@ fetch('https://dev.adalab.es/api/todo')
   console.log(data);
   const tasksUrl = data.results;
   paintList(tasksUrl);
+  tasks = tasksUrl;
+  console.log('tasks:', tasks)
+
 
 })
 
+const handleNewTask = (event) => {
+  console.log('tasks2: ', tasks)
+  event.preventDefault();
+  tasks = [];
 
-fetch(`https://dev.adalab.es/api/todo/${GITHUB_USER}`, {
-   method: 'POST',
-   headers: {'Content-Type': 'application/json'},
-   body: JSON.stringify(newTaskDataObject),
- })
-   .then((response) => response.json())
-   .then((data) => {
-     if (data.success) {
-       //Completa y/o modifica el código:
-       //Agrega la nueva tarea al listado
-       //Guarda el listado actualizado en el local storage
-       //Visualiza nuevamente el listado de tareas
-       //Limpia los valores de cada input
-     } else {
-       //muestra un mensaje de error.
-     }
-   });
+  // 1. Recoge el nombre de la tarea
+  const inputNewTaskValue = inputNewTask.value;
 
+  // 2. Crea un objeto para la nueva tarea
+  const newTask = {
+    name: inputNewTaskValue, // sustituye este string vacío por el nombre de la tarea nueva
+    completed: false,
+  };
 
+  // 3. Añade la nueva tarea al array de tareas
+  tasks.push(newTask);
+  console.log('tasks3:' ,tasks)
 
-
-
-
-
-// const handleNewTask = (event) => {
-//   event.preventDefault();
-
-//   // 1. Recoge el nombre de la tarea
-//   const inputNewTaskValue = inputNewTask.value;
-
-//   // 2. Crea un objeto para la nueva tarea
-//   const newTask = {
-//     name: inputNewTaskValue, // sustituye este string vacío por el nombre de la tarea nueva
-//     completed: false,
-//   };
-
-//   // 3. Añade la nueva tarea al array de tareas
-//   tasks.push(newTask);
-//   console.log(data)
-
-//   // 4. Vuelve a pintar las tareas
-// };
-// buttonAdd.addEventListener('click', handleNewTask);
+  // 4. Vuelve a pintar las tareas
+  paintList(tasks)
+};
+buttonAdd.addEventListener('click', handleNewTask);
